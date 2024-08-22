facc03f660ce2a98fee40d3aecd375c3d289d39e
Munir <smsirajulmonir@gmail.com>
2024-08-22 16:19:37 -0700

    dunamic routing added

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import profile from "@/public/images/prof.jpeg";
+import Image from 'next/image';
 const MissionPage = () => {
     return (
         <main className='mt-10'>
             <div>
                 This is MissionPage
             </div>
+            <Image src={profile} alt='profile'/>
         </main>
     );
 };
